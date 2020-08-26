7e296f28b946f5155bcc9bff7c4a4282cb810656
Lauren Dove <elledoveinc@gmail.com>
2020-08-25 23:11:54 -0400

    question model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,14 @@ person = Theme.create(name: "person")
 films = Theme.create(name: "films")
 spaceships = Theme.create(name: "spaceships")
 species = Theme.create(name: "species")
-vehicles = Theme.create(name: "vehicles")
+#vehicles = Theme.create(name: "vehicles")
 planets = Theme.create(name: "planets")
+#themes = Theme.create([{name: "person"}, {name: "films"}, {name: "spaceships"}, {name: "species"}, {name: "vehicles"}, {name: "planets"}])
+
+Question.create(a_question: "What is Darth Vaders true indentity?", theme_id: person.id)
+Question.create(a_question: "What is the first Star Wars movie to debut & what year?", theme_id: films.id)
+Question.create(a_question: "What is is the name of Han Solos ship?", theme_id: spaceships.id) 
+Question.create(a_question: "What species is Jabba Desilijic Tiure?", theme_id: species.id)
+Question.create(a_question: "What planet was Anakin discovered on?", theme_id: planets.id)
+
+
