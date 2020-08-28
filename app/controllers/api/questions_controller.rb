97b49edce68910301a9e79a6d4c9e1d498e1f64a
Lauren Dove <elledoveinc@gmail.com>
2020-08-27 23:59:31 -0400

    added create resource and method to create new question and render json.private params also set up

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,2 +1,26 @@
 class Api::QuestionsController < ApplicationController
+    def index
+        questions = Question.all
+        render json: questions
+    end
+
+
+    def create 
+        question = Question.new(question_params)
+        if question.save
+            render json: question
+        else
+            #will show an error message to the user
+            render json: {errors: question.errors.full_messages}
+        end
+    end
+
+
+
+
+
+    private
+    def question_params
+        params.require(:question).permit(:a_question, :theme_id)
+    end
 end
